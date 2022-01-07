460e1b90863b71a9500efb312e1835de902adc6b
Louise Wang <louise.w1997@gmail.com>
2022-01-07 16:30:54 +0100

    fix: remove auth check

--- src/api/search.ts
+++ src/api/search.ts
@@ -1,11 +1,10 @@
 import { QueryClientConfig } from '../types';
-import configureAxios, { verifyAuthentication } from './axios';
+import configureAxios from './axios';
 import { buildGetItemsByKeywordRoute } from './routes';
 
 const axios = configureAxios();
 
 /* eslint-disable import/prefer-default-export */
 export const getItemsByKeywords = async (range: string, keywords: string, { API_HOST }: QueryClientConfig) =>
-  verifyAuthentication(() =>
-    axios.get(`${API_HOST}/${buildGetItemsByKeywordRoute(range, keywords)}`).then(({ data }) => data),
+  (axios.get(`${API_HOST}/${buildGetItemsByKeywordRoute(range, keywords)}`).then(({ data }) => data)
 );
