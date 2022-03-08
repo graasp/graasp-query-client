68b7524267a09d2e46d567029124ad33b38a3229
spaenleh <basile.1358@gmail.com>
2022-03-08 11:50:49 +0100

    fix: remove console logs

--- src/utils/item.ts
+++ src/utils/item.ts
@@ -46,13 +46,8 @@ export const getDirectParentId = (path: string) => {
   return ids[parentIdx];
 };
 
-export const hashItemsIds = (ids?: UUID[]) => {
-  const hashedIds = ids
-    ? CryptoJS.SHA1([...ids].sort().join("")).toString()
-    : undefined;
-  console.log('queryclient', ids, hashedIds)
-  return hashedIds
-}
+export const hashItemsIds = (ids?: UUID[]) => ids ? CryptoJS.SHA1([...ids].sort().join("")).toString() : undefined;
+
 export const isError = (error: unknown) => {
   const errorObject = error as GraaspError;
   return errorObject?.statusCode;
