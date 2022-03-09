68a0e7a8d7669aaaa1c35f8775e964191088672d
spaenleh <basile.1358@gmail.com>
2022-03-09 09:05:04 +0100

    fix: test ws

--- src/ws/hooks/chat.ts
+++ src/ws/hooks/chat.ts
@@ -37,7 +37,7 @@ export const configureWsChatHooks = (
           const chatKey = buildItemChatKey(chatId);
           const current: Record<Chat> | undefined =
             queryClient.getQueryData(chatKey);
-
+          console.log(current, event)
           if (current) {
             switch (event.op) {
               case OPS.PUBLISH: {
@@ -51,6 +51,7 @@ export const configureWsChatHooks = (
               case OPS.DELETE: {
                 const mutation = current.update('messages', (messages) => messages.filter(m => m.id !== event.message.id));
                 queryClient.setQueryData(chatKey, mutation);
+                console.log(mutation)
                 break;
               }
               default:
