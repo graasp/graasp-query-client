308fbb7414f265ca55af656aaad7a66c38ce8099
pyphilia <pyphilia@gmail.com>
2022-03-30 10:45:51 +0200

    refactor: add public export item

--- src/mutations/itemExport.ts
+++ src/mutations/itemExport.ts
@@ -1,19 +1,22 @@
 import { QueryClient } from 'react-query';
 import * as Api from '../api';
 import { MUTATION_KEYS } from '../config/keys';
-import { downloadItemRoutine } from '../routines';
+import { exportItemRoutine } from '../routines';
 import { QueryClientConfig } from '../types';
 
 export default (queryClient: QueryClient, queryConfig: QueryClientConfig) => {
   const { notifier } = queryConfig;
 
+  /**
+   * @param options.public {boolean} force fallback to public endpoint
+   */
   queryClient.setMutationDefaults(MUTATION_KEYS.EXPORT_ZIP, {
-    mutationFn: (id) => Api.downloadItem(id, queryConfig).then((data) => data),
+    mutationFn: ({ id, options }) => Api.exportItem(id, queryConfig, options),
     onSuccess: () => {
-      notifier?.({ type: downloadItemRoutine.SUCCESS });
+      notifier?.({ type: exportItemRoutine.SUCCESS });
     },
     onError: (error) => {
-      notifier?.({ type: downloadItemRoutine.FAILURE, payload: { error } });
+      notifier?.({ type: exportItemRoutine.FAILURE, payload: { error } });
     },
   });
 };
