e468edcf96bbcb0e9b287138e71ea9ffb1d76bf6
spaenleh <spaenleh@gmail.com>
2023-03-02 09:30:40 +0100

    fix: narrow type to only the required API_HOST from QueryConfig in apps

--- src/api/apps.ts
+++ src/api/apps.ts
@@ -16,14 +16,14 @@ const axios = configureAxios();
 
 export const getApps = async ({
   API_HOST,
-}: QueryClientConfig): Promise<App[]> =>
+}: Pick<QueryClientConfig, 'API_HOST'>): Promise<App[]> =>
   verifyAuthentication(() =>
     axios.get(`${API_HOST}/${buildAppListRoute}`).then(({ data }) => data),
   );
 
 export const requestApiAccessToken = async (
   args: { id: UUID; app: string; origin: string },
-  { API_HOST }: QueryClientConfig,
+  { API_HOST }: Pick<QueryClientConfig, 'API_HOST'>,
 ): Promise<{ token: string }> => {
   const { id, app, origin } = args;
   return fallbackToPublic(
