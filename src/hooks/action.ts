f8dbaedc8eee7306a6b925cf01f3876917932ce0
Po-Jui Chang <rayacers@gmail.com>
2023-06-08 09:59:40 +0200

    feat: add aggregateActions hook

--- src/hooks/action.ts
+++ src/hooks/action.ts
@@ -4,34 +4,62 @@ import { UUID, convertJs } from '@graasp/sdk';
 import { ActionDataRecord } from '@graasp/sdk/frontend';
 
 import * as Api from '../api';
-import { buildActionsKey } from '../config/keys';
+import { buildActionsKey, buildAggregateActionsKey } from '../config/keys';
 import { QueryClientConfig } from '../types';
 
 export default (queryConfig: QueryClientConfig) => {
   const { defaultQueryOptions } = queryConfig;
 
-  return {
-    useActions: (
-      args: {
-        itemId: UUID;
-        view: string;
-        requestedSampleSize: number;
-      },
-      options?: { enabled?: boolean },
-    ) => {
-      const enabledValue =
-        (options?.enabled ?? true) &&
-        Boolean(args.itemId) &&
-        Boolean(args.requestedSampleSize);
-      return useQuery({
-        queryKey: buildActionsKey(args),
-        queryFn: () =>
-          Api.getActions(args, queryConfig).then(
-            (data) => convertJs(data) as ActionDataRecord,
-          ),
-        ...defaultQueryOptions,
-        enabled: enabledValue,
-      });
+  const useActions = (
+    args: {
+      itemId: UUID;
+      view: string;
+      requestedSampleSize: number;
     },
+    options?: { enabled?: boolean },
+  ) => {
+    const enabledValue =
+      (options?.enabled ?? true) &&
+      Boolean(args.itemId) &&
+      Boolean(args.requestedSampleSize);
+    return useQuery({
+      queryKey: buildActionsKey(args),
+      queryFn: () =>
+        Api.getActions(args, queryConfig).then(
+          (data) => convertJs(data) as ActionDataRecord,
+        ),
+      ...defaultQueryOptions,
+      enabled: enabledValue,
+    });
   };
+
+  const useAggregateActions = (
+    args: {
+      itemId: UUID;
+      view: string;
+      requestedSampleSize: number;
+      type: string;
+      countGroupBy: string[];
+      aggregateFunction: string;
+      aggregateMetric: string;
+      aggregateBy: string[];
+    },
+    options?: { enabled?: boolean },
+  ) => {
+    const enabledValue =
+      (options?.enabled ?? true) &&
+      Boolean(args.itemId) &&
+      Boolean(args.requestedSampleSize);
+    return useQuery({
+      queryKey: buildAggregateActionsKey(args),
+      queryFn: () =>
+        Api.getAggregateActions(args, queryConfig).then((data) =>
+          convertJs(data),
+        ),
+      ...defaultQueryOptions,
+      enabled: enabledValue,
+    });
+  };
+
+  return { useActions, useAggregateActions };
 };
