e36ba58283716b7711df58d6748f10b94f5c01c0
spaenleh <spaenleh@gmail.com>
2023-06-27 16:55:24 +0200

    fix: use `categoryId` instead of `categoryIds`

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -341,7 +341,7 @@ export const buildGetAllPublishedItemsRoute = (categoryIds?: UUID[]) =>
   `${ITEMS_ROUTE}/${COLLECTIONS_ROUTE}${
     categoryIds?.length
       ? qs.stringify(
-          { categoryIds },
+          { categoryId: categoryIds },
           {
             arrayFormat: 'repeat',
             addQueryPrefix: true,
