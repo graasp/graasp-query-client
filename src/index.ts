c7eaf7ef394cff4710a34748b3c4b563d0e1ef21
spaenleh <spaenleh@gmail.com>
2023-09-25 08:56:46 +0200

    fix: use fully specified paths for ESM

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-export { default as configureQueryClient } from './queryClient';
-export * as Api from './api';
-export * as routines from './routines';
-export { DATA_KEYS } from './config/keys';
-export { API_ROUTES } from './api/routes';
+export { default as configureQueryClient } from './queryClient.js';
+export * as Api from './api/index.js';
+export * as routines from './routines/index.js';
+export { DATA_KEYS } from './config/keys.js';
+export { API_ROUTES } from './api/routes.js';
 
-export * from './types';
-export { MockWebSocket } from './ws';
+export * from './types.js';
+export { MockWebSocket } from './ws/index.js';
