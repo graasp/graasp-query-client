8bbf535a15d5985d9d05c1610c5632377c9b4e7a
LinaYahya <linaebe0@gmail.com>
2023-11-07 17:16:48 +0100

    build: add edit profile mutation

--- src/mutations/libraryProfile.ts
+++ src/mutations/libraryProfile.ts
@@ -1,35 +1,48 @@
-import { SUCCESS_MESSAGES } from '@graasp/translations';
-
-import { useMutation, useQueryClient } from 'react-query';
+import { useMutation } from 'react-query';
 
 import * as Api from '../api';
 import { PostProfilePayloadType } from '../api';
-import { createItemRoutine } from '../routines';
+import { patchProfileRoutine, postProfileRoutine } from '../routines';
 import type { QueryClientConfig } from '../types';
 
 export default (queryConfig: QueryClientConfig) => {
   const { notifier } = queryConfig;
 
-  const usePostProfile = () => {
-    const queryClient = useQueryClient();
-    return useMutation(
+  const usePostProfile = () =>
+    useMutation(
       async (profileData: PostProfilePayloadType) =>
         Api.postProfile(profileData, queryConfig),
       {
         onSuccess: () => {
           notifier?.({
-            type: createItemRoutine.SUCCESS,
-            payload: { message: SUCCESS_MESSAGES.CREATE_ITEM },
+            type: postProfileRoutine.SUCCESS,
+            payload: { message: 'Your Data Saved Successfully' },
           });
         },
         onError: (error: Error) => {
-          notifier?.({ type: createItemRoutine.FAILURE, payload: { error } });
+          notifier?.({ type: postProfileRoutine.FAILURE, payload: { error } });
         },
       },
     );
-  };
+  const useEditProfile = () =>
+    useMutation(
+      (payload: Partial<PostProfilePayloadType>) =>
+        Api.editProfile(payload, queryConfig),
+      {
+        onSuccess: () => {
+          notifier?.({
+            type: patchProfileRoutine.SUCCESS,
+            payload: { message: 'Your Data Saved Successfully' },
+          });
+        },
+        onError: (error: Error) => {
+          notifier?.({ type: patchProfileRoutine.FAILURE, payload: { error } });
+        },
+      },
+    );
 
   return {
     usePostProfile,
+    useEditProfile,
   };
 };
---
+++ src/routines/libraryProfile.ts
@@ -0,0 +1,5 @@
+import createRoutine from './utils';
+
+export const geTProfileRoutine = createRoutine('GET_LIBRARY_PROFILE');
+export const postProfileRoutine = createRoutine('POST_LIBRARY_PROFILE');
+export const patchProfileRoutine = createRoutine('PATCH_LIBRARY_PROFILE');
