a14cbb422fa6c79fd07f6d60d28f6a64b1a38638
kim <kim.phanhoang@epfl.ch>
2024-01-31 13:58:07 +0100

    refactor: fix

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -458,14 +458,14 @@ export const buildGetItemsInMapRoute = ({
   lat2: number;
   lng1: number;
   lng2: number;
-  search: string[];
+  search?: string[];
 }) => {
   const params = new URLSearchParams();
   params.append('lat1', lat1.toString());
   params.append('lat2', lat2.toString());
   params.append('lng1', lng1.toString());
   params.append('lng2', lng2.toString());
-  search.forEach((s) => params.append('search', s));
+  search?.forEach((s) => params.append('search', s));
 
   const searchString = params.toString();
 
