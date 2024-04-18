ced904d7a8d7cb1841c451af3b9b62d0117fc975
Mariem <mariembencheikh71@gmail.com>
2024-04-18 09:18:11 +0200

    fix: fix variable name in useEditMember hook

--- src/mutations/member.test.ts
+++ src/mutations/member.test.ts
@@ -207,8 +207,8 @@ describe('Member Mutations', () => {
 
     it(`Successfully edit member's username and ensures trimming`, async () => {
       const newUsernameWithTrailingSpace = 'newname  ';
-      const trimmedUsrname = newUsernameWithTrailingSpace.trim();
-      const response = { ...member, name: trimmedUsrname };
+      const trimmedUsername = newUsernameWithTrailingSpace.trim();
+      const response = { ...member, name: trimmedUsername };
       // set random data in cache
       queryClient.setQueryData(memberKeys.current().content, member);
       const endpoints = [
@@ -235,7 +235,7 @@ describe('Member Mutations', () => {
       const payload = patchSpy.mock.calls[0][1] as { name?: string };
 
       // Assert that the name sent to the API is trimmed
-      expect(payload.name).toEqual(newUsernameWithTrailingSpace.trim());
+      expect(payload.name).toEqual(trimmedUsername);
 
       // verify cache keys
       const newData = queryClient.getQueryData<CompleteMember>(
