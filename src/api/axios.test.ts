61ebc7454a36b835a7b367c0108470ea2fa48dba
spaenleh <spaenleh@gmail.com>
2024-06-11 16:25:46 +0200

    fix: add tests and ensure we do not send a request when array is empty

--- src/api/axios.test.ts
+++ src/api/axios.test.ts
@@ -1,10 +1,10 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 
 import {
   UNAUTHORIZED_RESPONSE,
   buildResultOfData,
 } from '../../test/constants.js';
-import { splitRequestByIdsAndReturn } from './axios.js';
+import { splitRequestByIds, splitRequestByIdsAndReturn } from './axios.js';
 
 const chunkSize = 5;
 const ids = Array.from({ length: chunkSize * 3 + 1 }, (_, idx) =>
@@ -13,69 +13,75 @@ const ids = Array.from({ length: chunkSize * 3 + 1 }, (_, idx) =>
 const buildRequest = async (chunk: typeof ids) =>
   buildResultOfData(chunk, (el) => el);
 
+describe('splitRequestByIds', () => {
+  it('Does not send request for empty arrays', async () => {
+    const mockBuildRequests = vi.fn().mockResolvedValue('ok');
+    await splitRequestByIds([], 1, mockBuildRequests);
+    expect(mockBuildRequests).toBeCalledTimes(0);
+  });
+});
+
 describe('Axios Tests', () => {
-  describe('Axios Tests', () => {
-    it('Throw for empty responses', async () => {
-      const zero = 0;
-      expect(() =>
-        splitRequestByIdsAndReturn(ids.slice(0, zero), chunkSize, buildRequest),
-      ).rejects.toThrow();
-    });
-    it('result contains the correct number of responses', async () => {
-      const small = 2;
-      const result1 = await splitRequestByIdsAndReturn(
-        ids.slice(0, small),
-        chunkSize,
-        buildRequest,
-      );
-      expect(Object.values(result1!.data).length).toEqual(small);
+  it('Throw for empty responses', async () => {
+    const zero = 0;
+    expect(() =>
+      splitRequestByIdsAndReturn(ids.slice(0, zero), chunkSize, buildRequest),
+    ).rejects.toThrow();
+  });
+  it('result contains the correct number of responses', async () => {
+    const small = 2;
+    const result1 = await splitRequestByIdsAndReturn(
+      ids.slice(0, small),
+      chunkSize,
+      buildRequest,
+    );
+    expect(Object.values(result1!.data).length).toEqual(small);
 
-      const twice = chunkSize + 1;
-      const result2 = await splitRequestByIdsAndReturn(
-        ids.slice(0, twice),
-        chunkSize,
-        buildRequest,
-      );
-      expect(Object.values(result2!.data).length).toEqual(twice);
+    const twice = chunkSize + 1;
+    const result2 = await splitRequestByIdsAndReturn(
+      ids.slice(0, twice),
+      chunkSize,
+      buildRequest,
+    );
+    expect(Object.values(result2!.data).length).toEqual(twice);
 
-      const big = ids.length;
-      const result3 = await splitRequestByIdsAndReturn(
-        ids.slice(0, big),
-        chunkSize,
-        buildRequest,
-      );
-      expect(Object.values(result3!.data).length).toEqual(big);
-    });
+    const big = ids.length;
+    const result3 = await splitRequestByIdsAndReturn(
+      ids.slice(0, big),
+      chunkSize,
+      buildRequest,
+    );
+    expect(Object.values(result3!.data).length).toEqual(big);
+  });
 
-    it('throws if one of the request throws', async () => {
-      await expect(
-        splitRequestByIdsAndReturn(ids.slice(0, 2), 1, async ([id]) => {
-          if (id === '1') {
-            throw new Error();
-          }
-          return buildResultOfData([id]);
-        }),
-      ).rejects.toThrow();
-    });
+  it('throws if one of the request throws', async () => {
+    await expect(
+      splitRequestByIdsAndReturn(ids.slice(0, 2), 1, async ([id]) => {
+        if (id === '1') {
+          throw new Error();
+        }
+        return buildResultOfData([id]);
+      }),
+    ).rejects.toThrow();
+  });
 
-    it('throws if one of the request contains an error', async () => {
-      let thrownError;
+  it('throws if one of the request contains an error', async () => {
+    let thrownError;
 
-      try {
-        await splitRequestByIdsAndReturn(ids.slice(0, 2), 1, async ([id]) => {
-          if (id === '1') {
-            return buildResultOfData([], (el) => el, [UNAUTHORIZED_RESPONSE]);
-          }
-          return buildResultOfData([id]);
-        });
-      } catch (error) {
-        thrownError = error;
-      }
-
-      // throwIfArrayContainsErrorOrReturn encapsulates the error in response.data
-      expect(thrownError).toEqual({
-        response: { data: [UNAUTHORIZED_RESPONSE] },
+    try {
+      await splitRequestByIdsAndReturn(ids.slice(0, 2), 1, async ([id]) => {
+        if (id === '1') {
+          return buildResultOfData([], (el) => el, [UNAUTHORIZED_RESPONSE]);
+        }
+        return buildResultOfData([id]);
       });
+    } catch (error) {
+      thrownError = error;
+    }
+
+    // throwIfArrayContainsErrorOrReturn encapsulates the error in response.data
+    expect(thrownError).toEqual({
+      response: { data: [UNAUTHORIZED_RESPONSE] },
     });
   });
 });
