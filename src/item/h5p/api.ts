131f8b29c76addeda56495b204c6ec0824bf937c
kim <kim.phanhoang@epfl.ch>
2024-07-01 14:19:02 +0200

    refactor: revert

--- src/item/h5p/api.ts
+++ src/item/h5p/api.ts
@@ -19,8 +19,9 @@ export const importH5P = async (
     const { id, previousItemId, file } = args;
     const itemPayload = new FormData();
 
-    itemPayload.append('fields', JSON.stringify({}));
-
+    if (previousItemId) {
+      itemPayload.append('previousItemId', previousItemId);
+    }
     /* WARNING: this file field needs to be the last one,
      * otherwise the normal fields can not be read
      * https://github.com/fastify/fastify-multipart?tab=readme-ov-file#usage
@@ -28,7 +29,7 @@ export const importH5P = async (
     itemPayload.append('file', file);
     return axios
       .post<DiscriminatedItem>(
-        `${API_HOST}/${buildImportH5PRoute(id, previousItemId)}`,
+        `${API_HOST}/${buildImportH5PRoute(id)}`,
         itemPayload,
         {
           headers: { 'Content-Type': 'multipart/form-data' },
